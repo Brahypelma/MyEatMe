7aeda632bed3517d66975b7ec4c6d777bbc585c5
Senheizer <dnedima@gmail.com>
2015-08-10 11:41:17 +0400

    зеленый смайл теперь имеет свой класс унаследованый от смайл

---
+++ core/src/gameworld/GreenSmile.java
@@ -0,0 +1,19 @@
+package gameworld;
+
+/**
+ * Created by Senheizer on 06.08.2015.
+ */
+public class GreenSmile extends Smile {
+
+
+    public GreenSmile(float x, float y, int weight, int height,int color) {
+        super(x, y, weight, height,color);
+    }
+
+
+
+    }
+
+
+
+
